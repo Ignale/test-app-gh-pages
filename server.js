7950e193c553cc54a6d7f9855e760b4e9949ff14
Ignale <ignat.lex@gmail.com>
2023-01-18 11:46:47 +0300

    new changes

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const auth = require('json-server-auth')
+const server = jsonServer.create();
+
+const router = jsonServer.router("db.json"); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+server.use(auth);
+
+server.listen(port);
